e249068642b00b8e950bb95b0e2607ebd79954e1
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-Air.lan>
2023-01-07 00:37:19 -0500

    update

--- duckhunt.js
+++ duckhunt.js
@@ -7,3 +7,10 @@ window.onload = function () {
     daffy.classList.toggle("flap")
     }, 250)
 }
+
+let moveDuck = (daffy) => {
+  daffy.style.top = `${Math.random() * window.innerHeight}px`;
+  daffy.style.left = `${Math.random() * window.innerWidth}px`;
+};
+
+let timeClock = (daffy) => { setInterval(moveDuck, 240, daffy); }
